9261f375261332a78bad72eda843d49372428ff8
VChow <victorchow_507@hotmail.com>
2014-09-17 00:16:24 +0100

    Created basic Categories selection screen
    New app icon. Categories List View generated with non-functional event
    listeners.

---
+++ Android Application/src/com/team8/potatodoctor/CategoriesListActivity.java
@@ -0,0 +1,69 @@
+package com.team8.potatodoctor;
+
+import java.util.ArrayList;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+/*
+ * Populates and displays the list of Categories on the main screen.
+ */
+public class CategoriesListActivity extends Activity
+{
+	ArrayList<String> categoriesNameList;
+	
+	/*
+	 * Displays ArrayList of values onto the UI.
+	 */
+	public void onCreate(Bundle saveInstanceState)
+	{
+		Log.d("Problem Determination", "onCreate() ENTRY");
+		super.onCreate(saveInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		//Get the reference of ListViewCategories
+		final ListView categoriesList=(ListView)findViewById(R.id.listCategories);
+		
+		categoriesNameList = new ArrayList<String>();
+		getCategoriesNames();
+		
+		//Create the Adapter to display ArrayList onto ListView.
+		ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, categoriesNameList);
+		
+		//Set the Adapter onto the ListView.
+		categoriesList.setAdapter(arrayAdapter);
+		
+		//Register onClickListener to handle click events on each item.
+		categoriesList.setOnItemClickListener(new OnItemClickListener()
+		{
+			//Argument position gives the index of item which is clicked.
+			public void onItemClick(AdapterView<?> arg0, View v, int position, long arg3)
+			{
+				String selectedCategory=categoriesNameList.get(position);
+			}
+
+		});
+		Log.d("Problem Determination", "onCreate() EXIT");
+	}
+
+	/*
+	 * Populates the ArrayList with category values.
+	 */
+	private void getCategoriesNames() 
+	{
+		Log.d("Problem Determination", "getCategoriesNames() ENTRY");
+		categoriesNameList.add("Pests");
+		categoriesNameList.add("Viruses");
+		categoriesNameList.add("Nutrient Deficiencies");
+		categoriesNameList.add("Tutorials");
+		Log.d("Problem Determination", "getCategoriesNames() EXIT");
+	}
+	
+
+}
--- Android Application/src/com/team8/potatodoctor/MainActivity.java
+++ Android Application/src/com/team8/potatodoctor/MainActivity.java
@@ -1,22 +1,38 @@
 package com.team8.potatodoctor;
 
+import java.io.IOException;
+import java.util.LinkedList;
+
+import org.json.JSONException;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.util.Log;
 import android.view.Menu;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity 
+{
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	protected void onCreate(Bundle savedInstanceState) 
+	{
+		Log.d("Problem Determination", "onCreate() ENTRY");
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		Intent intentCategoriesList = new Intent(getApplicationContext(),CategoriesListActivity.class);
+		startActivity(intentCategoriesList);
+		Log.d("Problem Determination", "onCreate() EXIT");
 	}
 
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
+	public boolean onCreateOptionsMenu(Menu menu)
+	{
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
 
+	
 }
