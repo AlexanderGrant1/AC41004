082fbf263dcf580d4e3651b7fc2334bc27c444d1
Jekabs Stikans <iferegi@gmail.com>
2014-09-17 19:44:40 +0100

    Fixed image relation

--- web/root/protected/models/PlantLeaf.php
+++ web/root/protected/models/PlantLeaf.php
@@ -52,7 +52,7 @@ class PlantLeaf extends CActiveRecord
 		// NOTE: you may need to adjust the relation name and the related
 		// class name for the relations automatically generated below.
 		return array(
-			'images' => array(self::HAS_MANY, 'PlantLeafPhoto', 'PlantLeafgit sId'),
+			'images' => array(self::HAS_MANY, 'PlantLeafPhoto', 'PlantLeafId'),
 		);
 	}
 
