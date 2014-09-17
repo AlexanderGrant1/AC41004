f3c4fbfc81da6da7d1c1432a8e7745485578ae6b
Jekabs Stikans <iferegi@gmail.com>
2014-09-17 12:30:19 +0100

    Added image relation

--- web/root/protected/models/Disease.php
+++ web/root/protected/models/Disease.php
@@ -52,6 +52,7 @@ class Disease extends CActiveRecord
 		// NOTE: you may need to adjust the relation name and the related
 		// class name for the relations automatically generated below.
 		return array(
+			'images' => array(self::HAS_MANY, 'DiseasePhoto', 'DiseaseId'),
 		);
 	}
 
