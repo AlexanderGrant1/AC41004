0069c7d787de7d05aaf43375a07b389570abc956
Jekabs Stikans <iferegi@gmail.com>
2014-09-17 19:23:59 +0100

    Adding bootsrap

--- web/root/protected/views/layouts/main.php
+++ web/root/protected/views/layouts/main.php
@@ -16,48 +16,54 @@
 	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/ie.css" media="screen, projection" />
 	<![endif]-->
 
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/main.css" />
+	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/main_new.css" />
 	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/form.css" />
 
 	<title><?php echo CHtml::encode($this->pageTitle); ?></title>
 </head>
 
 <body>
+<div id="wrap">
+	<div class="container" id="page">
+			<div id="mainmenu">
+				<?php $this->widget('bootstrap.widgets.TbNavbar', array(
+				    'color' => TbHtml::NAVBAR_COLOR_INVERSE,
+				    'brandLabel' => CHtml::encode(Yii::app()->name),
+				    'collapse' => true,
+				    'items'=>array(
+				    			array(
+					    			'class' => 'bootstrap.widgets.TbNav',
+					    			'items' => array(
+									array('label'=>'Home', 'url'=>array('/')),
+									array('label'=>'Pests', 'url'=>array('pests/'), 'visible'=>!Yii::app()->user->isGuest),
+									array('label'=>'Plants / Leafs', 'url'=>array('/plants-leafs/'), 'visible'=>!Yii::app()->user->isGuest),
+									array('label'=>'Tubers', 'url'=>array('/tubers/'), 'visible'=>!Yii::app()->user->isGuest),
+									array('label'=>'Login', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest),
+									TbHtml::navbarSearchForm('#'),
+									array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('/site/logout'), 'visible'=>!Yii::app()->user->isGuest)
+								),
+				    		),
+						),
+				)); ?>
+			</div><!-- mainmenu -->
+			<?php if(isset($this->breadcrumbs)):?>
+				<?php $this->widget('bootstrap.widgets.TbBreadcrumb', array(
+					'links'=>$this->breadcrumbs,
+				)); ?><!-- breadcrumbs -->
+			<?php endif?>
 
-<div class="container" id="page">
 
-	<div id="header">
-		<div id="logo"><?php echo CHtml::encode(Yii::app()->name); ?></div>
-	</div><!-- header -->
+		<?php echo $content; ?>
 
-	<div id="mainmenu">
-		<?php $this->widget('zii.widgets.CMenu',array(
-			'items'=>array(
-				array('label'=>'Home', 'url'=>array('/site/index')),
-				array('label'=>'About', 'url'=>array('/site/page', 'view'=>'about')),
-				array('label'=>'Contact', 'url'=>array('/site/contact')),
-				array('label'=>'Login', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest),
-				array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('/site/logout'), 'visible'=>!Yii::app()->user->isGuest)
-			),
-		)); ?>
-	</div><!-- mainmenu -->
-	<?php if(isset($this->breadcrumbs)):?>
-		<?php $this->widget('zii.widgets.CBreadcrumbs', array(
-			'links'=>$this->breadcrumbs,
-		)); ?><!-- breadcrumbs -->
-	<?php endif?>
+		<div class="clear"></div>
 
-	<?php echo $content; ?>
+		<div id="footer">
+			Copyright &copy; <?php echo date('Y'); ?> by Team 8.<br/>
+			All Rights Reserved.<br/>
+			<?php echo Yii::powered(); ?>
+		</div><!-- footer -->
 
-	<div class="clear"></div>
-
-	<div id="footer">
-		Copyright &copy; <?php echo date('Y'); ?> by My Company.<br/>
-		All Rights Reserved.<br/>
-		<?php echo Yii::powered(); ?>
-	</div><!-- footer -->
-
-</div><!-- page -->
-
+	</div><!-- page -->
+</div>
 </body>
 </html>
