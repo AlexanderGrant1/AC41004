2bd4bce131983f78844c4206dc883956f7b78ff3
Jekabs Stikans <iferegi@gmail.com>
2014-09-18 14:42:29 +0100

    Added image upload functionalito the form

--- web/root/protected/views/pest/_form.php
+++ web/root/protected/views/pest/_form.php
@@ -9,6 +9,7 @@
 <?php $form=$this->beginWidget('CActiveForm', array(
 	'id'=>'pest-form',
 	'enableAjaxValidation'=>false,
+	'htmlOptions' => array('enctype' => 'multipart/form-data'),
 )); ?>
 
 	<p class="note">Fields with <span class="required">*</span> are required.</p>
@@ -25,6 +26,12 @@
 		<?php echo $form->labelEx($model,'Description'); ?>
 		<?php echo $form->textArea($model,'Description',array('rows'=>6, 'cols'=>50)); ?>
 		<?php echo $form->error($model,'Description'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'image'); ?>
+		<?php echo $form->fileField($model,'image'); ?>
+		<?php echo $form->error($model,'image'); ?>
 	</div>
 
 	<div class="row buttons">
