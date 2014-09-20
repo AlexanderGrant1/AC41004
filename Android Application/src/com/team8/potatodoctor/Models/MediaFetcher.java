803d7452a618ca6a3593e38971ceff8ca858b113
Alexander Grant <alexander.grant55@gmail.com>
2014-09-20 03:05:11 +0100

    File downloading added.  Code cleanup

--- Android Application/src/com/team8/potatodoctor/Models/MediaFetcher.java
+++ Android Application/src/com/team8/potatodoctor/Models/MediaFetcher.java
@@ -1,5 +1,73 @@
 package com.team8.potatodoctor.Models;
 
-public class MediaFetcher {
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
+import android.os.AsyncTask;
+import android.os.Environment;
+import android.util.Log;
+
+public class MediaFetcher extends AsyncTask<String, Void, String>
+{
+	@Override
+	/*
+	 * @param params first string is the url, second string is the type of request e.g POST, PUT, GET, DELETE
+	 * @see android.os.AsyncTask#doInBackground(java.lang.Object[])
+	 */
+	protected String doInBackground(String... params) 
+	{
+		return fetch(params[0]);
+	}
+	
+	//Referenced http://stackoverflow.com/questions/16414515/save-image-from-url-to-sdcard
+	public String fetch(String mediaUrl)
+	{
+		String filepath = "";
+		try
+		{   
+		  URL url = new URL(mediaUrl);
+		  HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+		  urlConnection.setRequestMethod("GET");
+		  urlConnection.setDoOutput(true);                   
+		  urlConnection.connect();                  
+		  File SDCardRoot = Environment.getExternalStorageDirectory().getAbsoluteFile();
+		  String filename="downloadedFile.png";   
+		  Log.i("Local filename:",""+filename);
+		  File file = new File(SDCardRoot,filename);
+		  if(file.createNewFile())
+		  {
+		    file.createNewFile();
+		  }                 
+		  FileOutputStream fileOutput = new FileOutputStream(file);
+		  InputStream inputStream = urlConnection.getInputStream();
+		  int totalSize = urlConnection.getContentLength();
+		  int downloadedSize = 0;   
+		  byte[] buffer = new byte[1024];
+		  int bufferLength = 0;
+		  while ( (bufferLength = inputStream.read(buffer)) > 0 ) 
+		  {                 
+		    fileOutput.write(buffer, 0, bufferLength);                  
+		    downloadedSize += bufferLength;                 
+		    Log.i("Progress:","downloadedSize:"+downloadedSize+"totalSize:"+ totalSize) ;
+		  }             
+		  fileOutput.close();
+		  if(downloadedSize==totalSize) filepath=file.getPath();    
+		} 
+		catch (MalformedURLException e) 
+		{
+		  e.printStackTrace();
+		} 
+		catch (IOException e)
+		{
+		  filepath=null;
+		  e.printStackTrace();
+		}
+		Log.i("filepath:"," "+filepath) ;
+		return filepath;
+	}
 }
