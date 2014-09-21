74f7fbb7050e5434a68300a1dc86bbf54dcc5af7
Alexander Grant <alexander.grant55@gmail.com>
2014-09-21 15:19:00 +0100

    Local folders added to constants class

--- Android Application/src/com/team8/potatodoctor/Constants.java
+++ Android Application/src/com/team8/potatodoctor/Constants.java
@@ -2,8 +2,14 @@ package com.team8.potatodoctor;
 
 public class Constants {
 
+	//External URLs
 	public static final String PEST_API_URL = "http://beberry.lv/potato/api/pest";
 	public static final String TUBER_API_URL = "http://beberry.lv/potato/api/tuber";
 	public static final String PLANT_LEAF_API_URL = "http://beberry.lv/potato/api/plantleaf";
 	public static final String TUTORIAL_API_URL = "http://beberry.lv/potato/api/tutorial";
+	
+	//Internal folder locations
+	public static final String LOCAL_PEST_FOLDER = "PotatoDoctor/Pests";
+	public static final String LOCAL_TUBER_FOLDER = "PotatoDoctor/Tubers";
+	public static final String LOCAL_PLANTLEAF_FOLDER = "PotatoDoctor/PlantLeaf";
 }
