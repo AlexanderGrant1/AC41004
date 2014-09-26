4c3a031c7c72af0c7061975456272b8fc0ddd973
VChow <victorchow_507@hotmail.com>
2014-09-26 19:10:22 +0100

    Added line to direct user to update.

--- Android Application/src/com/team8/potatodoctor/activities/CategoriesListActivity.java
+++ Android Application/src/com/team8/potatodoctor/activities/CategoriesListActivity.java
@@ -212,6 +212,7 @@ public class CategoriesListActivity extends Activity
 		{
 			public void onClick(DialogInterface dialog, int which) {
 				//Proceed and update.
+				startActivity(new Intent(getBaseContext(),UpdateActivity.class));
 			}
 		}) //End of .setPositiveButton()
 		
