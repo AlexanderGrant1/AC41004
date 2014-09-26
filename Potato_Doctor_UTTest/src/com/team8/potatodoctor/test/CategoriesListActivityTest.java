84a47dad7a5caa8e4cda866873eee65cfd835bed
VChow <victorchow_507@hotmail.com>
2014-09-26 08:41:25 +0100

    Setup Unit Test project.

---
+++ Potato_Doctor_UTTest/src/com/team8/potatodoctor/test/CategoriesListActivityTest.java
@@ -0,0 +1,33 @@
+package com.team8.potatodoctor.test;
+
+import org.junit.Test;
+
+import android.test.ActivityUnitTestCase;
+import com.team8.potatodoctor.activities.CategoriesListActivity;
+
+
+public class CategoriesListActivityTest extends ActivityUnitTestCase<CategoriesListActivity> {
+
+	public CategoriesListActivityTest(Class<CategoriesListActivity> activityClass) {
+		super(activityClass);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected void setUp() throws Exception
+	{
+		super.setUp();
+		
+	}
+	
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+	
+	protected void onTearDown() throws Exception
+	{
+		
+	}
+
+}
